bb0aab4c45d131d39ae92d9314eacbcc0b074abd
sakshiiii12 <sakshiiii761@gmail.com>
2025-06-22 15:33:36 +0530

    printprogram

--- SQL Practice 3.sql
+++ SQL Practice 3.sql
@@ -11,3 +11,8 @@ first_name VARCHAR (50),
 last_name VARCHAR (50),
 hire_date DATE
 );
+
+#2.Add a New Column
+#Add a column salary (DECIMAL(10,2)) to the existing employees table.
+ALTER TABLE employees
+ADD COLUMN salary INT NOT NULL DEFAULT 20000;
